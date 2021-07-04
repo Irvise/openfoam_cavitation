8af12c21aabf1a5ee0183845f0519002c6549dd2
lizat-i <ivan.lizat@outlook.de>
2021-07-04 13:08:43 +0200

    posProcessing overhaul

--- postProcessing/probeEvaluation.m
+++ postProcessing/probeEvaluation.m
@@ -14,10 +14,9 @@ semilogy(p_center(:,1),p_center(:,2)/ambientPressure,'k')
 hold on
 plot(p_center(:,1),p_center(:,3)/ambientPressure,'r')
 plot(p_center(:,1),p_center(:,4)/ambientPressure,'b')
-plot(p_center(:,1),p_center(:,5)/ambientPressure,'c')
-plot(p_center(:,1),p_center(:,6)/ambientPressure,'g')
+ 
 title('symetryComparison')
-legend('probe-1','probe-2','probe-3','closetocenter','bubbleSurface')
+legend('Radius =  7.07e-3','Radius =  2.12e-2','Radius =  4.71e-2' )
 xlabel('time in s')
 ylabel('p/po ')
 hold off
